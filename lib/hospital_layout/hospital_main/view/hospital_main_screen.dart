b322eae87bd1ab002559fccc5b8300bd5e0141d3
Emad-Pro <emadeadev@gmail.com>
2024-11-16 20:49:45 +0200

    Finshed hospital bottomnavibar finsh settings screen

--- lib/hospital_layout/hospital_main/view/hospital_main_screen.dart
+++ lib/hospital_layout/hospital_main/view/hospital_main_screen.dart
@@ -1,16 +1,33 @@
+import 'package:blood_donation/hospital_layout/hospital_main/view_model/cubit/hospital_main_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../core/di/service_lacator.dart';
 
 class HospitalMainScreen extends StatelessWidget {
   const HospitalMainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text('THIS IS HOSPITAL MAIN !!!!')],
-      ),
+    return BlocBuilder<HospitalMainCubit, HospitalMainState>(
+      bloc: getIt<HospitalMainCubit>(),
+      builder: (context, state) {
+        final cubit = getIt<HospitalMainCubit>();
+        return Scaffold(
+          // appBar: AppBar(
+          //   title: Text(cubit.titles[state.currentIndex]),
+          // ),
+          body: cubit.screens[state.currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            selectedItemColor: Theme.of(context).colorScheme.primary,
+            currentIndex: state.currentIndex,
+            onTap: (index) {
+              cubit.changeIndex(index);
+            },
+            items: cubit.items,
+          ),
+        );
+      },
     );
   }
 }
